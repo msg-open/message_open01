55c02db5a58e6cff74bd2e1f1a85430fc4f45f48
Dolet <doletbizhev@gmail.com>
2018-11-26 22:56:25 +0300

    nuxt init

---
+++ layouts/error.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">Страница не найдена</h1>
+    <h1 v-else>Произошла ошибка</h1>
+    <nuxt-link to="/">Вернуться на главную</nuxt-link>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default() {
+        return [];
+      }
+    }
+  }
+};
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      55555555555555
+    </v-flex>
+  </v-layout>
+</template>
